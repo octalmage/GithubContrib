22b24ae8c73b7b178560046f21b775b3e063416a
Jason Stallings <jason@stallin.gs>
2015-01-19 12:41:23 -0600

    Working app,

---
+++ index.js
@@ -0,0 +1,97 @@
+var async = require("async");
+var svg = require("svgutils").Svg;
+var jsonselect = require("jsonselect");
+var https = require("https");
+var fs = require("fs");
+
+var gui = require('nw.gui');
+
+var tray;
+
+// Give it a menu
+var menu = new gui.Menu();
+menu.append(new gui.MenuItem({label: 'Exit', click: function() 
+{
+    gui.App.quit();
+}, }));
+
+var tasks = [];
+
+setInterval(function checkcommits()
+{
+	check();
+}, 10000);
+
+check();
+
+function makemenu(color)
+{
+	if (tray!=null)
+	{
+		tray.remove();
+	}
+	tray = new gui.Tray({ icon: "assets/images/icon" + color + ".png" });
+	tray.menu = menu;
+}
+
+function remind()
+{
+	var options = {
+  		icon: "assets/images/icon38.png",
+  		body: "Don't forget to commit today!"
+ 	};
+
+	var notification = new Notification("GithubContrib", options);
+}
+
+function check()
+{
+	console.log("check");
+	tasks.push(function(done)
+	{
+		var request = https.get("https://github.com/users/octalmage/contributions/", function(response)
+		{
+
+			var file = fs.createWriteStream(".download.svg");
+			response.pipe(file);
+
+			file.on("finish", function()
+			{
+				done();
+			});
+		});
+	});
+
+	tasks.push(function(done)
+	{
+		svg.fromSvgDocument("./.download.svg", function(err, svg)
+		{
+			if (err)
+			{
+				throw new Error("SVG file not found or invalid.");
+			}
+
+			var json = svg.toJSON();
+
+			commits = jsonselect.match('.type:val("rect") ~ .data', json);
+
+			fs.unlinkSync('./.download.svg');
+
+			done(commits);
+		});
+	});
+
+	async.series(tasks, function(re)
+	{
+		count=re[re.length-1].count;
+		if (count<0)
+		{
+			makemenu("b");
+		}
+		else
+		{
+			makemenu("r");
+			remind();
+		}
+	});
+}
